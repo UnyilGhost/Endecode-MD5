3180e6f208e21a588e5a01ebb0753c8c6600ebbf
UnyilGhost <39154186+UnyilGhost@users.noreply.github.com>
2018-05-11 11:35:26 +0000

    Create MD5.sh

---
+++ MD5.sh
@@ -0,0 +1,64 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Endecode MD5"
+sleep 1
+echo "\033[33;1m Author: UnyilGhost"
+sleep 1
+echo "\033[32;1m ★contact: unyilghost@gmail.com"
+sleep 1
+echo "\033[34;1m ★PhantomGhost"
+sleep 1
+echo "\033[36;1m Silahkan Pilih :"
+echo "1. Encrypt"
+echo "2. Decrypt"
+echo "\033[30;1m3. Informasi Tool Ini"
+echo "0. Keluar"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+python2 1.py
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+python2 2.py
+fi
+
+if
+[ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+toilet -f slant --gay "Endecode"
+echo "\033[31;1mNama tools: Endecode MD5"
+sleep 1
+echo "\033[33;1mKarya: UnyilGhost"
+sleep 1
+echo "\033[32;1mVersi: v1"
+sleep 1
+echo "\033[36;1mTeam: ★PhantomGhost★"
+sleep 1
+echo "\033[31;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "UnyilGhost"
+echo " 4prili666host"
+echo "Security Cyber Art"
+echo "PhantomGhost"
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh md5.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
